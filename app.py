0fca8e76201373677aaf00d7fe7fe8e1540453dd
Emiliopb <emiliop.b@hotmail.com>
2024-11-15 20:23:38 +0100

    Openpyxl para la prueba sin db

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import numpy as np
 import pydeck as pdk
 from datetime import time
 # import geopandas as gpd
+import openpyxl
 
 st.title('Una silla al sol 🪑 ☀️')
 st.subheader('Encuentra una terraza en Madrid que le esté dando el Sol.')
